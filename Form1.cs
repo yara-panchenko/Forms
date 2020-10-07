599bc434b817a611693018d382ff2feed052ac49
opilane <opilane@HP-CZC6297B9K>
2020-10-07 10:19:58 +0300

    First Commit

--- Form1.cs
+++ Form1.cs
@@ -124,6 +124,7 @@ namespace Forms
                 tabControl.SelectedIndex = 2;//0,1,2
                 this.Controls.Add(tabControl);
 
+
             }
             else if (e.Node.Text == "MessageBox")
             {
